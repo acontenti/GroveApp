56fdbf2af39342802aec89bb358a0affbf15ac81
Alessandro Contenti <a.contenti@gmail.com>
2015-08-21 23:02:23 +0200

    clean

--- androidstudio/GroveApp/app/src/main/java/com/simongeilfus/grove/groveapp/GroveAppActivity.java
+++
@@ -1,7 +0,0 @@
-package com.simongeilfus.grove.groveapp;
-
-import org.libcinder.app.CinderNativeActivity;
-
-public class GroveAppActivity extends CinderNativeActivity {
-    static final String TAG = "GroveAppActivity";
-}
--- assets/Shaders/TriangleHeightMap.frag
+++
@@ -1,8 +0,0 @@
-#include "Shaders/Common.glsl"
-
-in float vPosY;
-out vec4 oHeight;
-
-void main(){
-    oHeight = vec4( vec3( vPosY ), 1.0 );
-}
