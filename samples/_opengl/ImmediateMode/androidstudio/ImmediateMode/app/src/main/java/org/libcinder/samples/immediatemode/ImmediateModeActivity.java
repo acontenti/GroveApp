59961f0fba0969dda892bcd5de3823bb7591ac8c
Alessandro Contenti <a.contenti@gmail.com>
2015-08-25 12:16:31 +0200

    removed useless folders

--- samples/_opengl/ImmediateMode/androidstudio/ImmediateMode/app/src/main/java/org/libcinder/samples/immediatemode/ImmediateModeActivity.java
+++
@@ -1,7 +0,0 @@
-package org.libcinder.samples.immediatemode;
-
-import android.app.NativeActivity;
-
-public class ImmediateModeActivity extends NativeActivity {
-    static final String TAG = "ImmediateModeActivity";
-}
--- samples/_opengl/InstancedTeapots/androidstudio/InstancedTeapots/app/src/main/java/org/libcinder/samples/instancedteapots/InstancedTeapotsActivity.java
+++
@@ -1,7 +0,0 @@
-package org.libcinder.samples.instancedteapots;
-
-import android.app.NativeActivity;
-
-public class InstancedTeapotsActivity extends NativeActivity {
-    static final String TAG = "InstancedTeapotsActivity";
-}
